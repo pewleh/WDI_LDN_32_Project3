3b9ac46d0d11564f8e2eb75d3eea588b235fe573
Fabian Feldberg <feldbergf@gmail.com>
2018-03-19 09:19:53 +0000

    deleted some files

--- config/environment.js
+++
@@ -1,6 +0,0 @@
-const env = process.env.NODE_ENV || 'dev';
-const port = process.env.PORT || 4000;
-const dbURI = process.env.MONGODB_URI || `mongodb://localhost/stargazer-${env}`;
-const secret = process.env.SECRETSTAR;
-
-module.exports = {env, port, dbURI, secret};
