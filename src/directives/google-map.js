1cc80d2955c05eb80a02b138ae8f101d48114fdc
Helena Charles <helsz@hotmail.co.uk>
2018-03-21 21:46:51 +0000

    directions on places show

--- src/directives/google-map.js
+++ src/directives/google-map.js
@@ -1,6 +1,9 @@
 /* global google */
 
 function googleMap() {
+
+  let currentLocation = {};
+
   return {
     restrict: 'E',
     replace: true,
@@ -10,11 +13,39 @@ function googleMap() {
     },
     link($scope, $element) {
 
-
       const map = new google.maps.Map($element[0], {
         zoom: 14,
         center: $scope.center
       });
+
+      navigator.geolocation.getCurrentPosition(pos => {
+        console.log(pos.coords.latitude);
+        console.log(pos.coords.longitude);
+        const userCurrentLat = pos.coords.latitude;
+        const userCurrentLng = pos.coords.longitude;
+
+        currentLocation = { lat: userCurrentLat, lng: userCurrentLng };
+        displayRoute();
+        $scope.$apply();
+      });
+
+      const directionsDisplay = new google.maps.DirectionsRenderer();
+      const directionsService = new google.maps.DirectionsService();
+      // const origin = currentLocation;
+      // console.log(origin);
+
+      directionsDisplay.setMap(map);
+
+      function displayRoute() {
+
+        directionsService.route({
+          origin: currentLocation,
+          destination: $scope.center,
+          travelMode: 'DRIVING'
+        }, (response) => {
+          directionsDisplay.setDirections(response);
+        });
+      }
 
       const marker = new google.maps.Marker({
         position: $scope.center,
